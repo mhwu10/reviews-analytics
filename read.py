69ed38430ccf4822ac7a66d6f4648840c69ef158
MH Wu <apple49755@gmail.com>
2022-04-07 19:36:50 +0100

    add word count

--- read.py
+++ read.py
@@ -8,15 +8,45 @@ with open('reviews.txt', 'r') as f:
             print(len(data))
 print('Reading is done, there are', len(data), 'comments')
 
+print(data[0])
+
+# word count
+wc = {} # word acount
+for d in data:
+    words = d.split(' ')
+    for word in words:
+        if word in wc: #if word not in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+for word in wc:
+    if wc[word] > 1000000:
+        print(word, wc[word])  
+
+print(len(wc))
+print
+
+while True:
+    word = input('what key word do you search: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, 'appears', wc[word], 'times')
+    else:
+        print('This word is not in the dictionary.')
+
+print('See you!')
+
+
+
+
+
+
 sum_len = 0
 for dline in data:
     sum_len = sum_len + len(dline) 
 
 print('The average length of comments is', sum_len / len(data), 'digitals')
-
-#print(data[0])
-#print('-----------------')
-#print(data[1])
 
 new = []
 for dline in data:
@@ -37,4 +67,3 @@ print(good)
 
 bad = ['bad' in dline for dline in data]
 print(bad)
-
